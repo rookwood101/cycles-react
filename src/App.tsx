5c336911d804d1452346f631c4c5e99514f08237
Alex Sadler <alexandersadler1@gmail.com>
2020-05-09 11:14:35 +0100

    Animate percentage circle smoothly

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import PercentageCircle from './PercentageCircle';
 import PercentageCircleSVG from './PercentageCircleSVG';
 
 export default class App extends React.PureComponent<{}, {percentage: number}>{
+  #lastFrameTime: number;
+
   public constructor(props: {}) {
     super(props);
     this.state = {
-      percentage: 50,
+      percentage: 0,
     };
+    this.#lastFrameTime = performance.now();
   }
 
-  readonly #sliderChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ percentage: parseFloat(e.target.value) });
+  public componentDidMount(): void {
+    requestAnimationFrame(this.#increment);
+  }
+
+  readonly #increment = (): void => {
+    const currentFrameTime = performance.now();
+    const deltaTime = currentFrameTime - this.#lastFrameTime;
+    this.#lastFrameTime = currentFrameTime;
+
+    this.setState({percentage: this.state.percentage % 100 + 0.01*deltaTime});
+    requestAnimationFrame(this.#increment);
+  }
+
+  readonly #sliderChange = (e: React.SyntheticEvent<HTMLInputElement>): void => {
+    this.setState({ percentage: e.currentTarget.valueAsNumber });
   }
 
   public render() {
     return (
       <div className="App">
-        <PercentageCircle percent={this.state.percentage} />
         <PercentageCircleSVG percent={this.state.percentage} radius={25}/>
-        <input type="range" min="0" max="100" onChange={this.#sliderChange}/>
+        <input type="range" min="0" max="100" step="0.01"
+               style={{width: "calc(100% - 6px)"}}
+               onMouseDown={this.#sliderChange} onChange={this.#sliderChange}
+               value={this.state.percentage}/>
       </div>
     );
   }
